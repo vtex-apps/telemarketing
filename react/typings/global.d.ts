38c1cbb81311afaeb93eef7e5aaa236f095216ea
matheusps <matheus.ps@icloud.com>
2019-02-27 11:08:20 -0300

    prettifing ts files as well

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -1,36 +1,36 @@
 interface Session {
-  loading: boolean,
-  refetch: () => void,
+  loading: boolean
+  refetch: () => void
   getSession: {
-    adminUserEmail: string,
-    adminUserId: string,
-    impersonable: boolean,
-    profile: Profile,
+    adminUserEmail: string
+    adminUserId: string
+    impersonable: boolean
+    profile: Profile
     impersonate: {
-      storeUserId: string,
-      storeUserEmail: string,
+      storeUserId: string
+      storeUserEmail: string
       profile: Profile
-    },
-  },
+    }
+  }
 }
 
 interface ProcessedSession {
-  attendantEmail: string,
-  canImpersonate: boolean,
-  client?: Client,
+  attendantEmail: string
+  canImpersonate: boolean
+  client?: Client
 }
 
 interface Profile {
-  document: string,
-  phone: string,
-  firstName: string,
-  lastName: string,
+  document: string
+  phone: string
+  firstName: string
+  lastName: string
   email: string
 }
 
 interface Client {
-  document: string,
-  phone: string,
-  name: string,
-  email: string,
+  document: string
+  phone: string
+  name: string
+  email: string
 }
--- react/typings/telemarketing.d.ts
+++ react/typings/telemarketing.d.ts
@@ -1,4 +1,4 @@
 declare module '*.css' {
-    const css: any
-    export default css
-  }
+  const css: any
+  export default css
+}
